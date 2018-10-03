3b943954a6a6b0f652abdff72c9a709252cdc17e
Tuomaskaup <43319049+Tuomaskaup@users.noreply.github.com>
2018-10-03 21:08:03 +0300

    Data.txt trained for Chatterbot

--- Chatterbot-testi/chatbot.py
+++ Chatterbot-testi/chatbot.py
@@ -10,20 +10,6 @@ chatbot = ChatBot(
 	trainer = 'chatterbot.trainers.ListTrainer'
 )
 
-
-conversation = [
-    "Hi",
-    "Hey!",
-    "How are you doing?",
-    "I'm doing great.",
-    "That is nice to hear",
-    "Thanks.",
-    "You're welcome."
-]
-
-chatbot.train(conversation)
-
-
 while True:
 	try:
 		bot_input = chatbot.get_response(None)
