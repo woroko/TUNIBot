ae71342ed1f3045d2e14818c1870776b1960b674
woroko <29544613+woroko@users.noreply.github.com>
2018-10-30 16:56:47 +0200

    course implementation search

--- TUNIBrain/parse_uta_databank.py
+++ TUNIBrain/parse_uta_databank.py
@@ -1,25 +1,42 @@
 import json
 
 class UTAJsonParser:
+    def parse_files(self, json_dir):
+        with open(json_dir + '/uta_course_implementations.json', 'r') as f:
+            self.course_implementations = json.load(f)
+
     def __init__(self, json_dir):
-        with open(json_dir + '/uta_toteutukset.json', 'r') as f:
-            self.toteutukset = json.load(f)
+        self.parse_files(json_dir)
 
-    '''def search_regular_course_implementation(id=None, name=None):
-        if id is not None:
-            for toteutus in self.toteutukset:
-                if toteutus['']'''
+    def search_regular_course_implementation(self, id=None, name=None):
+        courses = []
+        try:
+            if id is not None:
+                for course in self.course_implementations:
+                    if id.lower() == course['code'].lower():
+                        courses.append(course)
+            if name is not None:
+                for course in self.course_implementations:
+                    if name.lower() in course['name'].lower():
+                        courses.append(course)
+        except Exception as e:
+            print(e)
 
+        return courses
 
-def main():
-    parser = UTAJsonParser("jsons")
-    print(parser.toteutukset[0]['name'])
-    print(len(parser.toteutukset))
+def dump_course_codes(parser):
     with open("course_codes.txt", 'w') as f:
-        for course in parser.toteutukset:
+        for course in parser.course_implementations:
             code = course['code']
             if len(code) > 1:
                 f.write(code + "\n")
+
+def main():
+    parser = UTAJsonParser("jsons")
+    print(parser.course_implementations[0]['name'])
+    print(len(parser.course_implementations))
+    print(parser.search_regular_course_implementation(name='Filosofian historia'))
+
 
 
 if __name__ == "__main__":
