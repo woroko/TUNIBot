725c1d63ebccc07562f36dc0de4113f7a25450a5
Iris353 <iris.leinonen@gmail.com>
2018-11-06 15:56:22 +0200

    api calls

--- TUNIBrain/app.py
+++ TUNIBrain/app.py
@@ -2,6 +2,8 @@ from bottle import Bottle, run, \
      template, debug, static_file, request, post
 
 import os, sys
+import requests
+import json
 from inspect import getsourcefile
 from os.path import abspath
 
@@ -20,5 +22,16 @@ debug(True)
 def test():
     username = request.forms.get('username')
     print(username)
+    response = requests.get("api-address")
+    #response = '{"intent": {"confidence": 0.5}}'
+    jsonObj = json.loads(response)
+    #print(moi['intent']['confidence'])
+    receivedThreshold = jsonObj['intent']['confidence']
+    
+    if (receivedThreshold > THRESHOLD):
+      if (jsonObj['intent']['name'] == 'coursepoints'):
+         
+    else:
+      callCSClient()
     
 run(app, host='localhost', port=8080)
