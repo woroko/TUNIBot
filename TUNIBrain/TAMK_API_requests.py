38adcb2ca77e0523a5e6c3171d790cc3a8be8151
Tuomas Kauppinen <tuomaskaup@hotmail.com>
2018-11-06 15:13:14 +0200

    TAMK_API_requests.py fixed

--- TUNIBrain/TAMK_API_requests.py
+++ TUNIBrain/TAMK_API_requests.py
@@ -4,14 +4,16 @@ def tamk_API_data():
    #TAMK 
    #URL and other info for API request
    url = 'https://opendata.tamk.fi/r1/courseunit/search'
-   headers = {'Accept-Language':'en'}
-   info = {"codes": ["18YTIKO"]}
+   headers = {'Accept-Language':'en', 'content-type':'application/json'}
+   info = {"codes": ["IM00BR45"]}
    info = json.dumps(info)
    loaded_info = json.loads(info)
    #Makes API-request and saves data into json-file.
-   r = requests.get(url, auth=('***REMOVED***', ''), data=loaded_info, headers=headers)
-   j=json.loads(r.text)
-   print(j)
+   r = requests.post(url, auth=('***REMOVED***', ''), data=info, headers=headers)
+
+   print(r)
+   print(r.content)
+  
    
    file = open("jsons/TAMK.json","w") 
    file.write(r.content.decode('utf-8'))
