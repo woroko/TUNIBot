7bd8fee60799d0c8f42e0a1c35038c344b97483c
Tuomas Kauppinen <tuomaskaup@hotmail.com>
2018-11-06 18:09:30 +0200

    TAMK working

---
+++ TUNIBrain/TAMK_API_courseunits.py
@@ -0,0 +1,24 @@
+import requests, json
+
+#Gets courses info from TAMK API by course id
+def TAMK_API_courseunits(id):
+   #TAMK 
+   #URL and other info for API request
+   url = 'https://opendata.tamk.fi/r1/courseunit/search'
+   headers = {'Accept-Language':'en', 'content-type':'application/json'}
+   info = {"codes": [id]}
+   info = json.dumps(info)
+   loaded_info = json.loads(info)
+   
+   #Makes API-request and returns data in json-form.
+   r = requests.post(url, auth=('***REMOVED***', ''), data=info, headers=headers)
+   ret = json.loads(r.text)
+   return ret
+   
+   
+#Searches courses credits by its id
+def TAMK_credits(id):
+   info = TAMK_API_courseunits(id)
+   credits = info['courseUnits'][0]['credits']
+   return("Course "+str(id)+" is worth "+str(credits)+" credits.")
+print(TAMK_credits("IM00BR45"))
