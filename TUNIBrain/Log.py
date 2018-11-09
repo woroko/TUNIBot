5aac68383d124d4d66cb234a1d5794161c2b1cf8
Tuomaskaup <43319049+Tuomaskaup@users.noreply.github.com>
2018-11-09 17:09:09 +0200

    Minor changes to Log.py

--- TUNIBrain/Log.py
+++ TUNIBrain/Log.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 def successful_log(Question, Response, Intent_confidence, Intent_name, Top_entity_confidence, Top_entity_name, Source):
     currentDate = datetime.now()
-    currentDate = currentDate.strftime(' %H-%M-%S %d-%m-%Y')
+    currentDate = currentDate.strftime(' %H-%M-%S %d.%m.%Y')
     with open('Logs/Successful/Successful'+ str(currentDate) +'.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['Question'] + ['Response'] + ['Intent_confidence'] + ['Intent_name'] + ['Top_entity_confidence'] + ['Top_entity_name'] + ['Source'])
@@ -11,7 +11,7 @@ def successful_log(Question, Response, Intent_confidence, Intent_name, Top_entit
     
 def failed_log(Question, Response, Intent_confidence, Intent_name, Top_entity_confidence, Top_entity_name, Source):
     currentDate = datetime.now()
-    currentDate = currentDate.strftime(' %H-%M-%S %d-%m-%Y')
+    currentDate = currentDate.strftime(' %H-%M-%S %d.%m.%Y')
     with open('Logs/Failed/Failed'+ str(currentDate) +'.csv', 'w', newline='') as csvfile:
         writer = csv.writer(csvfile, delimiter=',')
         writer.writerow(['Question'] + ['Response'] + ['Intent_confidence'] + ['Intent_name'] + ['Top_entity_confidence'] + ['Top_entity_name'] + ['Source'])
