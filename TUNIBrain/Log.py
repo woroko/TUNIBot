50c754a6835c92f096a9ba03005b29b0b92a1968
woroko <29544613+woroko@users.noreply.github.com>
2018-11-26 16:23:40 +0200

    close log

--- TUNIBrain/Log.py
+++ TUNIBrain/Log.py
@@ -4,6 +4,7 @@ from datetime import datetime
 class Logger:
     def __init__(self, logs_on):
         if logs_on:
+            self.logs_on
             currentDate = datetime.now()
             currentDate = currentDate.strftime('%H-%M-%S %d.%m.%Y')
             self.csvfile_s = open('Logs/Successful/'+ str(currentDate) +'.csv', 'w', newline='')
@@ -12,16 +13,16 @@ class Logger:
             self.writer_f = csv.writer(self.csvfile_f, delimiter=',')
             self.writer_s.writerow(['Question'] + ['Response'] + ['Intent_confidence'] + ['Intent_name'] + ['Top_entity_confidence'] + ['Top_entity_name'] + ['Source'])
             self.writer_f.writerow(['Question'] + ['Response'] + ['Intent_confidence'] + ['Intent_name'] + ['Top_entity_confidence'] + ['Top_entity_name'] + ['Source'])
-        
-        
+
+
     def log_success(self, Question, Response, Intent_confidence, Intent_name, Top_entity_confidence, Top_entity_name, Source):
         self.writer_s.writerow([Question] + [Response] + [Intent_confidence] + [Intent_name] + [Top_entity_confidence] + [Top_entity_name] + [Source])
 
     def log_failed(self, Question, Response, Intent_confidence, Intent_name, Top_entity_confidence, Top_entity_name, Source):
         self.writer_f.writerow([Question] + [Response] + [Intent_confidence] + [Intent_name] + [Top_entity_confidence] + [Top_entity_name] + [Source])
 
-        
+
     def __del__(self):
-        if logs_on:
+        if self.logs_on:
             self.csvfile_s.close()
             self.csvfile_f.close()
